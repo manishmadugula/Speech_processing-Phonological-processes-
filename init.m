fe95420067b9df0166fc317d95aea37410a878ee
Manish <manish2296lion@gmail.com>
2016-06-14 16:21:53 +0530

    added segmented sounds

--- init.m
+++ init.m
@@ -1,5 +1,20 @@
-[y1,Fs1,bit1]=wavread('3.wav');
-[y2,Fs2,bit2]=wavread('4.wav');
+[y1,Fs1,bit1]=wavread('total/puje/correct/puje.wav');
+[y2,Fs2,bit2]=wavread('total/puje/incorrect/puche_2.wav');
 Tw = 30;                % analysis frame duration (ms)
 Ts = 15;                % analysis frame shift (ms)
-    
+y1 = y1 / max(y1);
+y2 = y2 / max(y2);
+subplot(1,2,1);
+plot(y1);
+subplot(1,2,2);
+plot(y2);
+figure;
+
+
+
+% Max value 100: 
+% y = y / max(y) * 100;
+% Max absolute value 8: 
+% y = y / max(abs(y)) * 8;
+% Energy normalization: 
+% y = y / sum(y.^2); 
---
+++ zfSig_dtwcomparision.m
@@ -0,0 +1,68 @@
+function [ minimum_distance,area,matrixX,matrixY,steps ] = zfSig_dtwcomparision( y1,y2,Fs1,Fs2,Tw,Ts )
+%DTW of glottal closure instant and finds the area and horizontal and vertical segments
+%y1 AND y2 are 2 signals and Fs1 and Fs2 are their sampling frequency,Tw-window length,Ts-window Shift
+%area is the area between y=x and the minimum path curve
+%matrixX and matrixY has 3 columns the length of the horizontal segment and
+%starting and ending co-ordinates of the segment in other 2 columns
+
+    [zfSig1, gci1, slope1]=epochExtract(y1, Fs1);
+    [zfSig2, gci2, slope2]=epochExtract(y2, Fs2);
+    subplot(3,2,1);
+    plot(zfSig1);
+    Frame_size=Tw/1000;
+    Frame_shift=Ts/1000;                             %so that x axis is time not samples
+    %% perform short term energy calculation of zfSig1
+    window_length=Frame_size*Fs1;
+    window_shift=floor(Frame_shift*Fs1);
+    sum1=0;
+    energy1=0;
+    w = window(@rectwin,window_length);
+    jj=1;
+    y=zfSig1;
+    for i=1:(floor(length(y)/window_shift)-ceil(window_length/window_shift))        %1 to 
+        for j=((i-1)*window_shift+1):(((i-1)*window_shift+1)+window_length-1)
+                y(j)=y(j)*w(jj);
+                jj=jj+1;
+                yy=y(j)*y(j);
+                sum1=sum1+yy;
+        end
+        energy1(i)=sum1;
+        sum1=0;
+        jj=1;
+    end
+    c1=energy1;
+    subplot(3,2,3);
+    plot(c1);
+    
+    subplot(3,2,2);
+    plot(zfSig2);
+    Frame_size=Tw/1000;
+    Frame_shift=Ts/1000;                             %so that x axis is time not samples
+    %% perform short term energy calculation of zfSig1
+    window_length=Frame_size*Fs2;
+    window_shift=floor(Frame_shift*Fs2);
+    sum2=0;
+    energy2=0;
+    w = window(@rectwin,window_length);
+    jj=1;
+    y=zfSig2;
+    for i=1:(floor(length(y)/window_shift)-ceil(window_length/window_shift))        %1 to 
+        for j=((i-1)*window_shift+1):(((i-1)*window_shift+1)+window_length-1)
+                y(j)=y(j)*w(jj);
+                jj=jj+1;
+                yy=y(j)*y(j);
+                sum2=sum2+yy;
+        end
+        energy2(i)=sum2;
+        sum2=0;
+        jj=1;
+    end
+    c2=energy2;
+    subplot(3,2,4);
+    plot(c2);
+    subplot(3,2,[5,6]);
+    [ minimum_distance,area,matrixX,matrixY,steps ] = dtwFeatureExtraction( c1,c2,Tw );
+
+    
+end
+
